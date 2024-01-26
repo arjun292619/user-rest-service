9965bbdb97cf4f5cfaa293cb4dd78b3256a38225
arjun292619 <j.c.arjunreddy@gmail.com>
2024-01-26 13:16:20 -0800

    refactor name

--- src/dev/lpa/collectionMethods/CollectionsFillAdd.java
+++ src/dev/lpa/collectionMethods/CollectionsFillAdd.java
@@ -2,7 +2,7 @@ package dev.lpa.collectionMethods;
 
 import java.util.*;
 
-public class Main {
+public class CollectionsFillAdd {
     public static void main(String[] args) {
         List<Card> deck = Card.getStandardDeck();
         Card.printDeck(deck);
