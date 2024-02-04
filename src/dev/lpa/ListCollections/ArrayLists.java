e475147208ac4d1c4b1df055da2f64c44d632a52
arjun292619 <j.c.arjunreddy@gmail.com>
2024-02-04 15:12:30 -0800

    ArrayLists add & remove and bulk add

--- src/dev/lpa/ListCollections/ArrayLists.java
+++ src/dev/lpa/ListCollections/ArrayLists.java
@@ -9,6 +9,11 @@ public class ArrayLists {
         private GroceryItem(String name) {
             this(name, "DAIRY", 1);
         }
+
+        @Override
+        public String toString() {
+            return "%d %s in %s".formatted(count, name.toUpperCase(), type);
+        }
     }
 
     public static void main(String[] args) {
@@ -17,7 +22,7 @@ public class ArrayLists {
         groceryArray[0] = new GroceryItem("Milk");
         groceryArray[1] = new GroceryItem("Apples", "PRODUCE", 6);
 //        groceryArray[2]= "5 Oranges";
-        groceryArray[2] = new GroceryItem("Oranges","PRODUCE",5);
+        groceryArray[2] = new GroceryItem("Oranges", "PRODUCE", 5);
         System.out.println(Arrays.toString(groceryArray));
 
         List objectList = new ArrayList<>();
@@ -30,9 +35,27 @@ public class ArrayLists {
         groceryList.add(new GroceryItem("Butter"));
         groceryList.add(new GroceryItem("Yogurt"));
 //        groceryList.add("Chicken");
-        groceryList.add(new GroceryItem("Chicken","MEAT",2));
+        groceryList.add(new GroceryItem("Chicken", "MEAT", 2));
+        groceryList.add(1, new GroceryItem("Hersheys", "Chocolate", 3));
+        groceryList.set(1, new GroceryItem("Fish", "Meat", 2));
+        groceryList.remove(2);
+        groceryList.remove(new GroceryItem("Fish", "Meat", 2));
         System.out.println(groceryList);
 
+        //using List.of for instantiation
+        String[] items = {"apples", "bananas", "milk", "eggs"};
+        String[] stationaryArray = {"books","pencil","paints","brush","papers"};
 
+        List<String> list = List.of(items);
+        System.out.println(list);
+        System.out.println(list.getClass().getName());
+//        list.add("yogurt");
+
+//        List<String> groceries = new ArrayList<>(list);
+        ArrayList<String> groceries = new ArrayList<>(List.of(items));
+        groceries.add("yogurt");
+        System.out.println(groceries.getClass().getName());
+        groceries.addAll(List.of(stationaryArray));
+        System.out.println(groceries);
     }
 }
