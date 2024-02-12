30614de17249eada0d0e51baad19ee2ad808b959
arjun292619 <j.c.arjunreddy@gmail.com>
2024-02-12 14:44:39 -0800

    abstraction declaration and usage

---
+++ src/dev/lpa/Abstraction/Fish.java
@@ -0,0 +1,26 @@
+package dev.lpa.Abstraction;
+
+public class Fish extends Animal {
+    public Fish(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+        if (speed.equalsIgnoreCase("slow")) {
+            System.out.println(type + " is gliding");
+        } else {
+            System.out.println(type + " is swimming");
+        }
+    }
+
+    @Override
+    public void makeNoise() {
+        if (type == "Goldfish") {
+            System.out.println("Swish! ");
+        } else {
+            System.out.println("Swash! ");
+        }
+    }
+
+}
