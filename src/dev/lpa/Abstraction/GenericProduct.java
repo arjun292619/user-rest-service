16b89b6a7c5f90781477339d4d034881af4f07fa
arjun292619 <j.c.arjunreddy@gmail.com>
2024-02-13 13:52:59 -0800

    abstract method challenge

---
+++ src/dev/lpa/Abstraction/GenericProduct.java
@@ -0,0 +1,23 @@
+package dev.lpa.Abstraction;
+
+public abstract class GenericProduct {
+    protected String type;
+    protected double price;
+    protected String description;
+
+    public GenericProduct(String type, double price, String description) {
+        this.type = type;
+        this.price = price;
+        this.description = description;
+    }
+
+    public double getSalesPrice(int quantity) {
+        return price * quantity;
+    }
+
+    public void printPricedItem(int quantity) {
+        System.out.printf("%2d qty at %8.2f each, %-15s %-35s%n", quantity, price, type, description);
+    }
+
+    public abstract void showDetails();
+}
