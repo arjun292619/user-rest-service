b89a91f173d5a7bf82cbcd9bd100c2b558400310
arjun292619 <j.c.arjunreddy@gmail.com>
2024-02-22 12:39:45 -0800

    StaticNested classes ann comparators

---
+++ src/dev/lpa/NestedClasses/Employee.java
@@ -0,0 +1,47 @@
+package dev.lpa.NestedClasses;
+
+import java.util.Comparator;
+
+public class Employee {
+    private String name;
+    private int id;
+    private int yearStarted;
+
+    public static class EmployeeComparator implements Comparator<Employee> {
+        String sortType;
+
+        public EmployeeComparator() {
+            this("name");
+        }
+
+        public EmployeeComparator(String sortType) {
+            this.sortType = sortType;
+        }
+
+        @Override
+        public int compare(Employee o1, Employee o2) {
+            return switch (this.sortType.toLowerCase()) {
+                case "name" -> o1.name.compareTo(o2.name);
+                case "yearstarted" -> Integer.compare(o1.yearStarted, o2.yearStarted);
+                default -> Integer.compare(o1.id, o2.id);
+            };
+//            return Integer.compare(o1.yearStarted, o2.yearStarted);
+        }
+    }
+
+    public Employee(int id, String name, int yearStarted) {
+        this.name = name;
+        this.id = id;
+        this.yearStarted = yearStarted;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+//        return "%d %-8s %d".formatted(id, name, yearStarted);
+        return String.format("%d %-8s %d", id, name, yearStarted);
+    }
+}
---
+++ src/dev/lpa/NestedClasses/StaticNested.java
@@ -0,0 +1,39 @@
+package dev.lpa.NestedClasses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StaticNested {
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>(List.of(
+                new Employee(10001, "Ralph", 2015),
+                new Employee(10009, "Carol", 2021),
+                new Employee(10024, "Uma", 2018),
+                new Employee(10024, "Hugh", 2016),
+                new Employee(10024, "Jim", 2014),
+                new Employee(10024, "Dwight", 2011),
+                new Employee(10024, "Michael", 2010)
+        ));
+
+        Employee[] employeesArray = new Employee[]{new Employee(10001, "Ralph", 2015),
+                new Employee(10009, "Carol", 2021),
+                new Employee(10024, "Uma", 2018),
+                new Employee(10024, "Hugh", 2016),
+                new Employee(10024, "Jim", 2014),
+                new Employee(10024, "Dwight", 2011),
+                new Employee(10024, "Michael", 2010)};
+
+        EmployeeComparator<Employee> employeeComparator = new EmployeeComparator<>();
+        System.out.println(employees);
+//        employees.sort(employeeComparator);
+        employees.sort(new Employee.EmployeeComparator("name"));
+//        employees.sort(new Employee.EmployeeComparator("yearStarted"));
+//        System.out.println(employees);
+        for (var employee : employees) {
+            System.out.println(employee);
+        }
+//        Arrays.sort(employeesArray,employeeComparator);
+//        System.out.println(Arrays.toString(employeesArray));
+
+    }
+}
